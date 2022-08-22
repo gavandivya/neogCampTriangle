1ec71ec6ef8d737ac170130cef7187c509a599aa
Divya gavandi <49062060+gavandivya@users.noreply.github.com>
2022-08-22 17:17:46 +0530

    feat:Added questions

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector('.quizForm');
 const quizBtn = document.querySelector('#quizBtn');
 const output = document.querySelector('#outputText');
 
-const correctAnswer = ["90°","Right Angled"];
+const correctAnswer = ["90°","Right Angled","Equilateral","30°","Obtuse","a + b + c"];
 
 
 function calculateScore(){
